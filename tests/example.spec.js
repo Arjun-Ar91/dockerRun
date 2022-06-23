e0920317651a45c5f8dbd888aab453af3b63891a
Arjun <arjun1120@gmail.com>
2022-06-23 08:29:04 +0100

    asdds

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,10 +1,10 @@
-const { test, expect } = require('@playwright/test');
-
-test('basic test', async ({  }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
-  console.log("ARJUN Done Buddy", new Date().getHours(),"::", new Date().getMinutes());
-});
+// const { test, expect } = require('@playwright/test');
+//
+// test('basic test', async ({  }) => {
+//   await page.goto('https://playwright.dev/');
+//   const title = page.locator('.navbar__inner .navbar__title');
+//   await expect(title).toHaveText('Playwright');
+//   console.log("ARJUN Done Buddy", new Date().getHours(),"::", new Date().getMinutes());
+// });
 
 console.log("ARJUN Done Buddy", new Date().getHours(),"::", new Date().getMinutes());
