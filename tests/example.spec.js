775ff14e2af5a1911a0831d2b10127ed3f81c1bf
Arjun <arjun1120@gmail.com>
2022-06-23 11:14:38 +0100

    asdds

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,7 +4,7 @@ var os = require('os');
 test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright1');
+  await expect(title).toHaveText('Playwright');
     console.log("ARJUN Done Buddy", new Date().getHours(),"::", new Date().getMinutes());
 
     console.log(os.release()); // "10.0.14393"
