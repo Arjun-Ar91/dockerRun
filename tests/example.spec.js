2f4923bf9d36b11f09f9846de11dfa8fda94b482
Arjun <arjun1120@gmail.com>
2022-06-23 10:57:23 +0100

    asdds

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,7 +4,7 @@ var os = require('os');
 test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+  await expect(title).toHaveText('Playwright1');
     console.log("ARJUN Done Buddy", new Date().getHours(),"::", new Date().getMinutes());
 
     console.log(os.type()); // "Windows_NT"
